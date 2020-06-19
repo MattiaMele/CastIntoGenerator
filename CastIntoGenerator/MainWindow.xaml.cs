9b1ae2b5a206f4a8c5eeaae2133a1f9dc8615912
Mattia Mele <co.17.tia@gmail.com>
2020-06-19 16:35:59 +0200

    First commit

---
+++ CastIntoGenerator/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using CastIntoGeneratorBiz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CastIntoGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _Genera(object sender, RoutedEventArgs e)
+        {
+            _GetCode();
+        }
+        private void _GeneraClipboard(object sender, RoutedEventArgs e)
+        {
+            _GetCodeFromClipboard();
+        }
+        private void _GetCode()
+        {
+            try
+            {
+                TextBoxOutput.Document.Blocks.Clear();
+                string ans = CastIntoCodeGenerator.GetCode(_StringFromRichTextBox(TextBoxInput));
+                if (string.IsNullOrWhiteSpace(ans))
+                {
+                    _GetCodeFromClipboard();
+                    return;
+                }
+                Clipboard.SetText(ans);
+                TextBoxOutput.AppendText(ans);
+            }
+            catch (Exception ex)
+            {
+                TextBoxOutput.AppendText(ex.Message);
+                TextBoxOutput.AppendText(ex.StackTrace);
+            }
+        }
+        private void _GetCodeFromClipboard()
+        {
+            string messaggioErrore = "Inserire o copiare le propieta' dal quale generare il metodo";
+            try
+            {
+                TextBoxOutput.Document.Blocks.Clear();
+                string ans;
+                string cliboard = Clipboard.GetText();
+                if (cliboard == messaggioErrore) { return; }
+                if (string.IsNullOrWhiteSpace(cliboard))
+                {
+                    TextBoxOutput.AppendText(messaggioErrore);
+                    return;
+                }
+                else
+                {
+                    TextBoxInput.AppendText(cliboard);
+                    ans = CastIntoCodeGenerator.GetCode(cliboard);
+                }
+                Clipboard.SetText(ans);
+                TextBoxOutput.AppendText(ans);
+            }
+            catch (Exception)
+            {
+                TextBoxOutput.AppendText(messaggioErrore);
+                return;
+            }
+        }
+        private string _StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                // TextPointer to the start of content in the RichTextBox.
+                rtb.Document.ContentStart,
+                // TextPointer to the end of content in the RichTextBox.
+                rtb.Document.ContentEnd
+            );
+
+            // The Text property on a TextRange object returns a string
+            // representing the plain text content of the TextRange.
+            return textRange.Text;
+        }
+    }
+}
---
+++ CastIntoGeneratorBiz/CastIntoCodeGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastIntoGeneratorBiz
+{
+    public static class CastIntoCodeGenerator
+    {
+        public static string GetCode(string codicePropieta)
+        {
+            if (string.IsNullOrWhiteSpace(codicePropieta)) { return null; }
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("        public static T CastInto<T>(this TYPE input, T output = default) where T : TYPE, new()");
+            sb.AppendLine("        {");
+            sb.AppendLine("            if (output == null)");
+            sb.AppendLine("                output = new T();");
+            sb.AppendLine();
+            sb.AppendLine("            BASETYPE.CastInto(input, output);");
+            sb.AppendLine();
+
+            string[] righe = codicePropieta.Trim().Split('\n');
+            foreach(string riga in righe)
+            {
+                if (string.IsNullOrWhiteSpace(riga)) { continue; }
+                string[] rigaSplittata = riga.Trim().Split(' ');
+                if(rigaSplittata.Length > 2 && rigaSplittata[2].StartsWith("{"))
+                { sb.AppendLine($"            output.{rigaSplittata[1]}=input.{rigaSplittata[1]};"); }
+                else if (rigaSplittata.Length > 3 && rigaSplittata[0] == "public" && rigaSplittata[3].StartsWith("{"))
+                { sb.AppendLine($"            output.{rigaSplittata[2]}=input.{rigaSplittata[2]};"); }
+            }
+
+            sb.AppendLine();
+            sb.AppendLine("            return output;");
+            sb.AppendLine("        }");
+
+            return sb.ToString();
+        }
+    }
+
+}
