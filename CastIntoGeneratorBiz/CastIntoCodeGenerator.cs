741053f2336d5b2ed9b9966c81a238da9ec810b0
Mattia Mele <co.17.tia@gmail.com>
2020-11-16 23:55:04 +0100

    Added advanced parsing
    Now it will correctly clone Lists and arrays, and will try to use CastInto on non-basic types

--- CastIntoGeneratorBiz/CastIntoCodeGenerator.cs
+++ CastIntoGeneratorBiz/CastIntoCodeGenerator.cs
@@ -10,6 +10,72 @@ namespace CastIntoGeneratorBiz
 {
     public static class CastIntoCodeGenerator
     {
+        /// <summary>
+        /// if set to false property types will be ignored
+        /// </summary>
+        public static bool AdvancedParsing = true;
+
+        static string[] __BaseTypes = new string[]
+        {
+            "Byte"
+            ,"SByte"
+            ,"Int32"
+            ,"UInt32"
+            ,"Int16"
+            ,"UInt16"
+            ,"Int64"
+            ,"UInt64"
+            ,"Single"
+            ,"Double"
+            ,"Char"
+            ,"Boolean"
+            ,"String"
+            ,"Decimal"
+            ,"DateTime"
+            ,"TimeSpan"
+            ,"Guid"
+        };
+        static string[] __BaseTypesAliases = new string[]
+        {
+            "byte"
+            ,"sbyte"
+            ,"int"
+            ,"uint"
+            ,"short"
+            ,"ushort"
+            ,"long"
+            ,"ulong"
+            ,"float"
+            ,"double"
+            ,"char"
+            ,"bool"
+            ,"string"
+            ,"decimal"
+        };
+        static string[] __BaseTypesNamesCombination;
+        static string[] _baseTypesNames
+        {
+            get
+            {
+                if (__BaseTypesNamesCombination == null)
+                {
+                    __BaseTypesNamesCombination = __BaseTypesAliases
+                        .Concat(__BaseTypesAliases.Select(tipo => $"{tipo}?"))
+                        .Concat(__BaseTypesAliases.Select(tipo => $"Nullable<{tipo}>"))
+                        .Concat(__BaseTypesAliases.Select(tipo => $"System.Nullable<{tipo}>"))
+                        .Concat(__BaseTypes)
+                        .Concat(__BaseTypes.Select(Struct => $"{Struct}?"))
+                        .Concat(__BaseTypes.Select(Struct => $"System.{Struct}?"))
+                        .Concat(__BaseTypes.Select(Struct => $"Nullable<{Struct}>"))
+                        .Concat(__BaseTypes.Select(Struct => $"Nullable<System.{Struct}>"))
+                        .Concat(__BaseTypes.Select(Struct => $"System.Nullable<{Struct}>"))
+                        .Concat(__BaseTypes.Select(Struct => $"System.Nullable<System.{Struct}>"))
+                        .ToArray();
+                }
+                return __BaseTypesNamesCombination;
+            }
+        }
+
         public static string GetCode(string codicePropieta)
         {
             if (string.IsNullOrWhiteSpace(codicePropieta)) { return null; }
@@ -47,9 +113,9 @@ namespace CastIntoGeneratorBiz
                     sb.AppendLine();
                 }
                 else if (rigaSplittata.Length > 2 && rigaSplittata[2].StartsWith("{"))
-                { sb.AppendLine($"            output.{rigaSplittata[1]}=input.{rigaSplittata[1]};"); }
+                { sb.AppendLine(_GetCastLine(rigaSplittata[1], rigaSplittata[0])); }
                 else if (rigaSplittata.Length > 3 && rigaSplittata[0] == "public" && rigaSplittata[3].StartsWith("{"))
-                { sb.AppendLine($"            output.{rigaSplittata[2]}=input.{rigaSplittata[2]};"); }
+                { sb.AppendLine(_GetCastLine(rigaSplittata[2], rigaSplittata[1])); }
             }
 
             sb.AppendLine();
@@ -100,6 +166,37 @@ namespace CastIntoGeneratorBiz
             }
 
             return (className, baseClassesList);
+        }
+        private static string _GetCastLine(string nomePropieta, string tipoPropieta)
+        {
+            if(AdvancedParsing == false)
+            {
+                return $"            output.{nomePropieta}=input.{nomePropieta};";
+            }
+            if (tipoPropieta.StartsWith("List<")) 
+            {
+                string type = tipoPropieta.Substring(5, tipoPropieta.Length - 6);
+                if (_baseTypesNames.Contains(type)) 
+                { return $"            output.{nomePropieta}=input.{nomePropieta}.ToList();"; }
+                else 
+                { return $"            output.{nomePropieta}=input.{nomePropieta}.Select(x=>x.CastInto(new {type}())).ToList();"; }
+            }
+            else if (tipoPropieta.EndsWith("[]"))
+            {
+                string type = tipoPropieta.Substring(0, tipoPropieta.Length - 2);
+                if (_baseTypesNames.Contains(type))
+                { return $"            output.{nomePropieta}=input.{nomePropieta}.ToArray();"; }
+                else
+                { return $"            output.{nomePropieta}=input.{nomePropieta}.Select(x=>x.CastInto(new {type}())).ToArray();"; }
+            }
+            else
+            {
+                if (_baseTypesNames.Contains(tipoPropieta))
+                { return $"            output.{nomePropieta}=input.{nomePropieta};"; }
+                else
+                { return $"            output.{nomePropieta}=input.{nomePropieta}.CastInto(new {tipoPropieta}());"; }
+                   
+            }
         }
         
     }
---
+++ CastIntoGeneratorBiz/TestBed.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastIntoGeneratorBiz
+{
+    public class Test1
+    {
+        public int intero { get; set; }
+        public double? doubles { get; set; }
+        public string stringa { get; set; }
+        public List<int> ListaInt { get; set; }
+
+    }
+    public class Test2 : Test1
+    {
+        public Guid[] GuidArray { get; set; }
+        public Test1 ComplexType { get; set; }
+    }
+    public class Test3 : Test2
+    {
+        public List<Test1> ListaOggetti { get; set; }
+        public Test1[] ArrayOggetti { get; set; }
+    }
+
+    public static class Extensions
+    {
+        public static T CastInto<T>(this Test1 input, T output = default) where T : Test1, new()
+        {
+            if (output == null)
+                output = new T();
+
+            output.intero = input.intero;
+            output.doubles = input.doubles;
+            output.stringa = input.stringa;
+            output.ListaInt = input.ListaInt.ToList();
+
+            return output;
+        }
+        public static T CastInto<T>(this Test2 input, T output = default) where T : Test2, new()
+        {
+            if (output == null)
+                output = new T();
+
+            (input as Test1).CastInto(output);
+
+            output.GuidArray = input.GuidArray.ToArray();
+            output.ComplexType = input.ComplexType.CastInto(new Test1());
+
+            return output;
+        }
+
+        public static T CastInto<T>(this Test3 input, T output = default) where T : Test3, new()
+        {
+            if (output == null)
+                output = new T();
+
+            (input as Test2).CastInto(output);
+
+            output.ListaOggetti = input.ListaOggetti.Select(x => x.CastInto(new Test1())).ToList();
+            output.ArrayOggetti = input.ArrayOggetti.Select(x => x.CastInto(new Test1())).ToArray();
+
+            return output;
+        }
+
+    }
+}
